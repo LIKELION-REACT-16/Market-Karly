77d25d37bf782b7639b9ee4e07b85b6d6caa501a
no-support <tk2ektk3ek@gmail.com>
2023-03-14 23:04:17 +0900

    [feat] InputCheckbox comp 마크업, 스타일링 #58

---
+++ src/components/InputCheckbox/InputCheckbox.jsx
@@ -0,0 +1,59 @@
+import { useId } from 'react';
+import { A11yHidden } from '@/components';
+import classes from './InputCheckbox.module.scss';
+
+export function InputCheckbox({
+  label,
+  type = 'checkbox',
+  invisibleLabel = false,
+  invisibleInput = true,
+  vertical = false,
+  ...restProps
+}) {
+  const id = useId();
+  const combineClassNames = `${classes.control} ${
+    vertical ? classes.control_vertical : ''
+  }`;
+
+  return (
+    <div className={combineClassNames}>
+      {renderInput(id, type, invisibleInput, restProps)}
+      {renderLabel(id, label, invisibleLabel)}
+    </div>
+  );
+}
+
+function renderLabel(id, label, invisibleLabel) {
+  return invisibleLabel ? (
+    <A11yHidden as="label" htmlFor={id} className={classes.label}>
+      {label}
+    </A11yHidden>
+  ) : (
+    <label htmlFor={id} className={classes.label}>
+      {label}
+    </label>
+  );
+}
+
+function renderInput(id, type, invisibleInput, restProps) {
+  return invisibleInput ? (
+    <A11yHidden
+      as="input"
+      id={id}
+      type={type}
+      className={classes.input}
+      {...restProps}
+    ></A11yHidden>
+  ) : (
+    <input id={id} type={type} className={classes.input} {...restProps} />
+  );
+}
+
+/* --------------------------------- Example -------------------------------- */
+{
+  /* <InputCheckbox
+  label={'비밀글로 문의하기'}
+  type="checkbox"
+  invisibleInput={true}
+/>; */
+}
