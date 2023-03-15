3c52b2a702130b72d96cd3767d3fc389fb37547f
SJM <warning6715@gmail.com>
2023-03-15 17:12:53 +0900

    [refact] inputtext props추가 #60

--- src/components/InputText/InputText.jsx
+++ src/components/InputText/InputText.jsx
@@ -1,8 +1,8 @@
 import classes from './InputText.module.scss'
 
-function InputText(){
+function InputText(props){
   return(
-    <input type="text" className={classes.text}/>
+    <input type="text" className={classes.text} placeholder={props.detail} />
   );
 }
 
