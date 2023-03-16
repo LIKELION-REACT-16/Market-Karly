40fc4285d0257a06697b5e7b05c23d78e9f20953
ukssss <1872003115su@gmail.com>
2023-03-17 02:40:08 +0900

    [refact] Button 컴포넌트 마크업 및 레이아웃 전체 수정 #102
    - Sass 모듈 고려한 마크업 및 레이아웃 수정
    - NormalButton, PopupButton, FooterButton 제작

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,60 @@
-import '@/styles/components/button.scss';
+import classes from '@/styles/components/Button.module.scss';
 
-const Button = (props) => {
-  const { label, styleClass, onClick, disabled } = props;
+export function NormalButton(props) {
+  const {
+    children,
+    color = false,
+    width,
+    height,
+    borderType = false,
+    ...restProps
+  } = props;
+  const combineClassName = `${classes.normal} ${color ? classes.white : ''} ${
+    width === '143'
+      ? classes.width143
+      : width === '333'
+      ? classes.width333
+      : width === '340'
+      ? classes.width340
+      : width === '424'
+      ? classes.width424
+      : width === '155'
+      ? classes.width155
+      : width === '244'
+      ? classes.width244
+      : width === '284'
+      ? classes.width284
+      : width === '174'
+      ? classes.width174
+      : ''
+  } ${height === '54' ? classes.height54 : classes.height44} ${
+    borderType ? classes.grayborder : ''
+  }`;
 
   return (
-    <button className={styleClass} onClick={onClick} disabled={disabled}>
-      {label}
+    <button type="button" className={combineClassName}>
+      {children}
     </button>
   );
-};
+}
 
-export default Button;
+export function PopupButton(props) {
+  const { children, color, ...restProps } = props;
+  const combineClassName = `${classes.popup} ${color ? classes.gray : ''}`;
+
+  return (
+    <button type="button" className={combineClassName}>
+      {children}
+    </button>
+  );
+}
+
+export function FooterButton(props) {
+  const { children, ...restProps } = props;
+
+  return (
+    <button type="button" className={classes.footer}>
+      {children}
+    </button>
+  );
+}
