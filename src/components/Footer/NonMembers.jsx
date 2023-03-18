2b649fa50bc759794d499b8977f3d8c549700fa1
ukssss <1872003115su@gmail.com>
2023-03-18 23:21:04 +0900

    [feat] Footer 컴포넌트 CustomerService 추가 작업 #110
    - Sass Module 추가
    - LinkMalito, NonMembers 컴포넌트 제작

--- src/components/Footer/NonMembers.jsx
+++ src/components/Footer/NonMembers.jsx
@@ -2,29 +2,15 @@ import classes from '@/styles/components/Footer/NonMembers.module.scss';
 import { Link } from 'react-router-dom';
 
 import Arrow from '@/assets/Arrow.svg';
+import LinkMailto from './LinkMailto';
 
 export default function NonMembers(props) {
   const { children, mailto, ...restProps } = props;
 
   return (
     <div className={classes.frame}>
-      <span>{children}&nbsp;</span>
-      <ButtonMailto mailto={mailto}>{mailto}</ButtonMailto>
+      <span className={classes.text}>{children}&nbsp;</span>
+      <LinkMailto mailto={mailto}>{mailto}</LinkMailto>
     </div>
-  );
-}
-
-function ButtonMailto({ mailto }) {
-  return (
-    <Link
-      to="#"
-      onClick={(e) => {
-        window.location.href = `mailto:${mailto}`;
-        e.preventDefault();
-      }}
-    >
-      <span>{mailto}</span>
-      <img className="More" src={Arrow} alt="더보기" />
-    </Link>
   );
 }
