4a1b9232d4938d2935b65f85d92c2ef9791b46db
ukssss <1872003115su@gmail.com>
2023-03-18 23:24:48 +0900

    [feat] Footer 컴포넌트 About 파트 #110
    - FooterNav, Area, SocialNav 제작 및 조립
    - 누락한 Footer.jsx 추가

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+import classes from '@/styles/components/Footer/Footer.module.scss';
+
+import CustomerService from './CustomerService';
+import About from './About';
+import Guarantee from './Guarantee';
+import Copyright from './Copyright';
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className={classes.inner}>
+          <div className={classes.innerTop}>
+            <CustomerService />
+            <About />
+          </div>
+          <div className={classes.innerBot}>
+            <Guarantee />
+          </div>
+        </div>
+        <div className={classes.bot}>
+          <div className={classes.botInner}>
+            <Copyright />
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
