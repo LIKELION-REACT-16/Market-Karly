88f03321d50325b98a4fbf5202fb20e50fb5cd4d
donghyeon413 <ghhrn413@naver.com>
2023-03-20 14:18:26 +0900

    [feat] Banner 마크업&스타일링 구현 #3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
-import Test from '@/components/test';
 import RouteLink from '@/components/RouteLink';
-
-import { Routes, Route } from 'react-router-dom';
+import banner03 from '@/assets/img-banner03.svg';
+import { Banner } from '@/components/Banner/Banner';
 
 const Home = () => {
   return (
     <div>
       <h2>Home</h2>
+      <Banner imgSrc={banner03} children="더 풍성해진 10월의 퍼플위크 적립률 UP+3종 쿠폰팩
+  KURLY PURPLE WEEK"/>
       <RouteLink />
     </div>
   );
