aca341f4c90348e7fe8b6ea701e228f75de83e87
donghyeon413 <ghhrn413@naver.com>
2023-03-21 23:38:46 +0900

    [feat] main | Slider 마크업,스와이퍼 기능구현  #2,#15

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,16 @@ import Test from '@/components/test';
 import RouteLink from '@/components/RouteLink';
 
 import { Routes, Route } from 'react-router-dom';
-import Footer from '@/components/Footer/Footer';
+// import Footer from '@/components/Footer/Footer';
+import { Slider } from '@/components/Slider/Slider';
+import React from "react";
+import ReactDOM from "react-dom";
 
 const Home = () => {
   return (
-    <div>
+    <div style={{height:'100.3vh'}}>
       <h2>Home</h2>
+      <Slider/>
     </div>
   );
 };
