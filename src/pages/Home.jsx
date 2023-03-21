384ada900857d08346ed3641c055e8e36d1d2fae
donghyeon413 <ghhrn413@naver.com>
2023-03-21 23:38:46 +0900

    [feat] main | Slider 마크업,스와이퍼 기능구현  #2,#15

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import RouteLink from '@/components/RouteLink';
 
 import { Routes, Route } from 'react-router-dom';
+
+import { Routes, Route } from 'react-router-dom';
 // import Footer from '@/components/Footer/Footer';
 import { Slider } from '@/components/Slider/Slider';
 import React from 'react';
@@ -8,14 +10,10 @@ import ReactDOM from 'react-dom';
 
 const Home = () => {
   return (
-    <>
+    <div style={{ height: '100.3vh' }}>
+      <h2>Home</h2>
       <Slider />
-      <Banner
-        imgSrc={banner03}
-        children="더 풍성해진 10월의 퍼플위크 적립률 UP+3종 쿠폰팩
-      KURLY PURPLE WEEK"
-      />
-    </>
+    </div>
   );
 };
 
