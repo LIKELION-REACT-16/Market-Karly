a7251eda0830d9f634a28b6606dfcdae69451655
donghyeon413 <ghhrn413@naver.com>
2023-03-21 23:38:46 +0900

    [feat] main | Slider 마크업,스와이퍼 기능구현  #2,#15

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -18,6 +18,7 @@ import classes from '@/components/Slider/Slider.module.scss';
 
 // import required modules
 import { Pagination, Navigation } from 'swiper';
+import { Pagination, Navigation } from 'swiper';
 
 // 이미지 파일 가져오기
 import img1 from '@/assets/img-banner01.svg';
@@ -35,9 +36,10 @@ export function Slider() {
   const prevButton = useRef(null);
   const nextButton = useRef(null);
 
-  // const style = {backgroundColor: "red"}
+  const style = { backgroundColor: 'red' };
   return (
     <>
+      <button ref={prevButton} className={classes.swiperPrevBtn} />
       <Swiper
         // navigation={true}
         navigation={{
@@ -59,7 +61,6 @@ export function Slider() {
         className={classes.mySwiper}
         // className={classes}
       >
-        <button ref={prevButton} className={classes.swiperPrevBtn} />
         <SwiperSlide>
           <img className={classes.Sliderimage} src={img4} alt="" />
         </SwiperSlide>
@@ -72,9 +73,8 @@ export function Slider() {
         <SwiperSlide>
           <img className={classes.Sliderimage} src={img1} alt="" />
         </SwiperSlide>
-
-        <button ref={nextButton} className={classes.swiperNextBtn} />
       </Swiper>
+      <button ref={nextButton} className={classes.swiperNextBtn} />
     </>
   );
 }
