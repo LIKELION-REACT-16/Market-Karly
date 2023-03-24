667ed3115b2f760b910636a07be448307221db32
Byeonjin <quswlstkd88@naver.com>
2023-03-24 17:25:16 +0900

    [fix] Detail | 다른 페이지로 이동시 라우팅 오류 해결 #147
    - 페이지가 언마운트 시 cleanup 방식 변경(IntersectionObserver.prototype.disconnect())

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -16,6 +16,10 @@ const ProductDetail = () => {
   const refList = [description, detailInformation, review, inquiry];
 
   const onIntersect = (entries) => {
+    console.log(entries.length);
+
+    if (entries.length == 0) return;
+
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         switch (entry.target) {
@@ -48,9 +52,7 @@ const ProductDetail = () => {
     });
 
     return () => {
-      refList.map((item) => {
-        observer.observe(item.current);
-      });
+      observer.disconnect();
     };
   }, []);
 
