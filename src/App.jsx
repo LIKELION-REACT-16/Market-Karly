5dbf50611429a401ee4591f75131d8fa053dbdec
ukssss <1872003115su@gmail.com>
2023-03-25 12:21:19 +0900

    [Setting] reactstrap 설치

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {
   Route,
 } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const Layout = lazy(() => import('./components/Layout/Layout'));
 const Home = lazy(() => import('./pages/Home'));
