463bc5895213e7d96696f529714864b0c9cc843f
ukssss <1872003115su@gmail.com>
2023-03-25 12:38:44 +0900

    [Setting] reactstrap 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import {
   Route,
 } from 'react-router-dom';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
 
 const Layout = lazy(() => import('./components/Layout/Layout'));
 const Home = lazy(() => import('./pages/Home'));
